2985fd7217b7d0b05602b3a6c1a3fd17f10932e6
Matteo Sandrin <sandrin98@gmail.com>
2023-05-03 13:52:14 -0400

    Run formatter on cobalt-clicker

--- cobalt-clicker.user.js
+++ cobalt-clicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cobalt-clicker
 // @namespace    http://sandrin.dev
-// @version      1.0.0
+// @version      1.0.1
 // @description  When navigating to gadeloitte.com, automatically click on the button to access Cobalt
 // @author       https://github.com/matteosandrin
 // @match        https://app3.gadeloitte.com/*
@@ -10,65 +10,65 @@
 // ==/UserScript==
 
 function getTextNodesIn(elem, filterFunction) {
-    var textNodes = [];
-    if (elem) {
-        for (var nodes = elem.childNodes, i = nodes.length; i--;) {
-            var node = nodes[i], nodeType = node.nodeType;
-            if (nodeType == 3) {
-                if (!filterFunction || filterFunction(node, elem)) {
-                    textNodes.push(node);
-                }
-            }
-            else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
-                textNodes = textNodes.concat(getTextNodesIn(node, filterFunction));
-            }
+  var textNodes = [];
+  if (elem) {
+    for (var nodes = elem.childNodes, i = nodes.length; i--; ) {
+      var node = nodes[i],
+        nodeType = node.nodeType;
+      if (nodeType == 3) {
+        if (!filterFunction || filterFunction(node, elem)) {
+          textNodes.push(node);
         }
+      } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+        textNodes = textNodes.concat(getTextNodesIn(node, filterFunction));
+      }
     }
-    return textNodes;
+  }
+  return textNodes;
 }
 
 function isImmigrationNode(node, parent) {
-    return /Enter your immigration details/.test(node.nodeValue);
+  return /Enter your immigration details/.test(node.nodeValue);
 }
 
 function waitUntilPageReady() {
-    var navbar = document.querySelector("mat-sidenav");
-    if (!navbar) {
-        console.log("[0] page is not ready yet, waiting...");
-        setTimeout(waitUntilPageReady, 200);
-        return;
-    }
-    var matching_text_nodes = getTextNodesIn(navbar, isImmigrationNode);
-    if (matching_text_nodes.length == 0) {
-        console.log("[1] page is not ready yet, waiting...");
-        setTimeout(waitUntilPageReady, 200);
-        return;
-    }
-    console.log("[2] page is ready!");
-    clickImmigrationButton();
+  var navbar = document.querySelector("mat-sidenav");
+  if (!navbar) {
+    console.log("[0] page is not ready yet, waiting...");
+    setTimeout(waitUntilPageReady, 200);
+    return;
+  }
+  var matching_text_nodes = getTextNodesIn(navbar, isImmigrationNode);
+  if (matching_text_nodes.length == 0) {
+    console.log("[1] page is not ready yet, waiting...");
+    setTimeout(waitUntilPageReady, 200);
+    return;
+  }
+  console.log("[2] page is ready!");
+  clickImmigrationButton();
 }
 
 function clickImmigrationButton() {
-    var navbar_buttons = document.querySelectorAll("mat-list-item");
-    for (let i = 0; i < navbar_buttons.length; i++) {
-        var button = navbar_buttons[i];
-        var matching_text_nodes = getTextNodesIn(button, isImmigrationNode);
-        if (matching_text_nodes.length > 0) {
-            console.log("[3] clicking immigration button");
-            button.click();
-            closeThisTab();
-            return;
-        }
+  var navbar_buttons = document.querySelectorAll("mat-list-item");
+  for (let i = 0; i < navbar_buttons.length; i++) {
+    var button = navbar_buttons[i];
+    var matching_text_nodes = getTextNodesIn(button, isImmigrationNode);
+    if (matching_text_nodes.length > 0) {
+      console.log("[3] clicking immigration button");
+      button.click();
+      closeThisTab();
+      return;
     }
-    console.log("[5] ERROR: could not find button to click");
+  }
+  console.log("[5] ERROR: could not find button to click");
 }
 
 function closeThisTab() {
-    console.log("[4] closing this tab");
-    setTimeout(window.close, 200);
+  console.log("[4] closing this tab");
+  setTimeout(window.close, 200);
 }
 
-(function() {
-    'use strict';
-    setTimeout(waitUntilPageReady, 200);
+(function () {
+  "use strict";
+  setTimeout(waitUntilPageReady, 200);
 })();
